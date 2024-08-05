1a9a7083f2589f7a71303be7e5dead15289feab5
meganhuibregtse <114030237+meganhuibregtse@users.noreply.github.com>
2024-08-05 12:46:26 -0400

    Update sphere.py
    Added calculation for volume of a sphere for issue #24

---
+++ src/geometry/sphere.py
@@ -0,0 +1,20 @@
+from math import pi
+
+def calculate_volume(r):
+  """ 
+    Calculate the volume of a sphere. 
+
+    This uses the standard mathematical calculation: 
+
+    $\(4/3) pi r ^3$
+
+    Parameters
+    ----------
+    r : float, the radius 
+
+    Returns
+    -------
+    float : the volume of the sphere
+    """
+    volume = (4/3) * pi * r **3
+    return volume
